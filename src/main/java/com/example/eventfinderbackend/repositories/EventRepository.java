159d6fd68dbfc81fe9eb419d2b261ee00d598c40
sha5hw4t <shashwat_shetty@hotmail.com>
2018-12-09 04:38:19 -0500

    event service and repository implemented

---
+++ src/main/java/com/example/eventfinderbackend/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.example.eventfinderbackend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.eventfinderbackend.models.Event;
+
+public interface EventRepository  extends CrudRepository<Event, Integer>{
+
+}
